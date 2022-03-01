5c7d8ee880c1d10c2d6278a0a7f1410c643a523c
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-03-02 00:13:23 +0100

    paging with remote only

--- app/src/main/java/com/amarinag/marvelapi/data/source/MarvelCharacterRemoteDataSource.kt
+++ app/src/main/java/com/amarinag/marvelapi/data/source/MarvelCharacterRemoteDataSource.kt
@@ -1,9 +1,12 @@
 package com.amarinag.marvelapi.data.source
 
-import com.amarinag.marvelapi.domain.model.Character
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import com.amarinag.marvelapi.data.network.MarvelApiService
 import com.amarinag.marvelapi.data.network.model.toModel
 import com.amarinag.marvelapi.di.AppDispatchers
+import com.amarinag.marvelapi.domain.model.Character
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -32,4 +35,29 @@ class MarvelCharacterRemoteDataSource @Inject constructor(
                 Result.failure(IllegalArgumentException("Network Exception"))
             }
         }
+}
+
+@Singleton
+class CharacterPagingSource @Inject constructor(
+    private val marvelApiService: MarvelApiService
+) : PagingSource<Int, Character>() {
+    override fun getRefreshKey(state: PagingState<Int, Character>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Character> = try {
+        val nextPageNumber = params.key ?: 0
+        delay(5000)
+        val response = marvelApiService.getAllCharacter(nextPageNumber)
+        LoadResult.Page(
+            data = response.data?.results.toModel(),
+            prevKey = null,
+            nextKey = response.data?.offset?.plus(response.data.limit)
+        )
+    } catch (ex: Exception) {
+        LoadResult.Error(ex)
+    }
 }
--- app/src/main/java/com/amarinag/marvelapi/ui/home/HomeRoute.kt
+++ app/src/main/java/com/amarinag/marvelapi/ui/home/HomeRoute.kt
@@ -1,13 +1,12 @@
 package com.amarinag.marvelapi.ui.home
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.paging.compose.collectAsLazyPagingItems
 
-@ExperimentalFoundationApi
 @Composable
 fun HomeRoute(homeViewModel: HomeViewModel, navigateToDetail: (Long) -> Unit) {
     val uiState by homeViewModel.uiState.collectAsState()
-    HomeScreenWithGrid(uiState, navigateToDetail)
+    HomeScreenWithList(uiState, navigateToDetail)
 }
