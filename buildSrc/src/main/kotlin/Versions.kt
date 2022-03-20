1beb62876890c7e9572a208c5fa4684173e36266
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-03-20 11:46:01 +0100

    migrate settings.gradle to kts

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,17 @@
+object Versions {
+    const val compose = "1.1.1"
+    const val accompanist = "0.24.2-alpha"
+    const val navigation = "2.4.1"
+    const val hilt = "2.40.5"
+    const val hiltNavigationCompose = "1.0.0"
+    const val retrofit = "2.9.0"
+    const val loggingInterceptor = "5.0.0-alpha.2"
+    const val room = "2.4.2"
+    const val truth = "1.1.3"
+    const val coil = "1.4.0"
+    const val mockk = "1.12.2"
+    const val lottie = "5.0.1"
+    const val mockWebservice = "5.0.0-alpha.2"
+    const val paging = "3.1.0"
+    const val leakcanary = "2.8.1"
+}
